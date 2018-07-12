f03eaff8a3d043b8f0fce8be43980b92e72ee7e2
slothman5566 <ccps960316@gmail.com>
2018-07-12 17:06:56 +0800

    edit binarization

--- Binarization/main.py
+++ Binarization/main.py
@@ -3,24 +3,49 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-image = mpimg.imread('./lena.bmp')
-fig = plt.figure()
-viewer=fig.add_subplot(2,1,1)
-viewer.axes.get_yaxis().set_visible(False)
-viewer.axes.get_xaxis().set_visible(False)
-viewer.set_title('before')
-plt.imshow(image)
+class Binarization:
+    #Gray= R * 0.299 + G * 0.578 + B * 0.114
+    def process(self,image):
+        
+        newImage=np.zeros(image.shape)
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                gray=image[x,y][0]* 0.299+image[x,y][1]* 0.578+image[x,y][2]* 0.114 
+                newImage[x,y]=[225,255,255] if gray>120 else [0,0,0]
+        return newImage
 
-# R * 0.299 + G * 0.578 + B * 0.114
-newImage=np.zeros(image.shape)
-for x in range(image.shape[0]):
-    for y in range(image.shape[1]):
-        color=image[x,y][0]* 0.299+image[x,y][1]* 0.578+image[x,y][2]* 0.114 
-        newImage[x,y]=[225,255,255] if color>120 else [0,0,0]
+    def process_with_threshold(self, image):
+        newImage=np.zeros(image.shape)
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                gray=image[x,y][0]* 0.299+image[x,y][1]* 0.578+image[x,y][2]* 0.114 
+                if(gray>200):
+                    newImage[x,y]=[225,255,255]
+                elif(gray>150):
+                    newImage[x,y]=[0,0,0]
+                elif(gray>100):
+                    newImage[x,y]=[225,255,255]
+                elif(gray>50):
+                    newImage[x,y]=[0,0,0]
+                
+        return newImage
 
-viewer=fig.add_subplot(2,1,2)
-viewer.axes.get_yaxis().set_visible(False)
-viewer.axes.get_xaxis().set_visible(False)
-viewer.set_title('after')
-plt.imshow(newImage)
-plt.show()
+def show_image(image_list):
+    fig = plt.figure()
+    fig.set_size_inches(10,10)
+    for i,v in enumerate(image_list):
+        viewer=fig.add_subplot(len(image_list),1,i+1)
+        viewer.axes.get_yaxis().set_visible(False)
+        viewer.axes.get_xaxis().set_visible(False)
+        plt.imshow(v)
+    plt.margins(0.05, 0.1)
+    plt.show()
+
+if __name__ == '__main__':
+    image = mpimg.imread('./image/lena.bmp')
+    processor=Binarization()    
+    image_list=[]
+    image_list.append(image)
+    image_list.append(processor.process(image))
+    image_list.append(processor.process_with_threshold(image))
+    show_image(image_list)
