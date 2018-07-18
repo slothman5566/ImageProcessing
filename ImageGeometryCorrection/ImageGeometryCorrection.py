1cf70cfa1d22a2ae174db1565bbb3f711b930d62
slothman5566 <ccps960316@gmail.com>
2018-07-18 21:01:14 +0800

    update interpolation

--- ImageGeometryCorrection/ImageGeometryCorrection.py
+++ ImageGeometryCorrection/ImageGeometryCorrection.py
@@ -31,22 +31,44 @@ class Transformation:
                 new_image[x,y]=image[x,image.shape[1]-y-1]
         return new_image.astype(np.uint8)
 
-    def scale_transform(self, image,sacle):    
+    def nearestInterpolation_transform(self, image,sacle):    
         new_image=np.zeros(image.shape)
-        for x in range(image.shape[0]):
-            sx=int(x*sacle)
+        resize_width=int(image.shape[0]*sacle)
+        resize_height=int(image.shape[1]*sacle)
+        new_image=np.zeros((resize_width,resize_height,3))
+        for x in range(resize_width):
+            sx=int(x/sacle)
             if(sx>=image.shape[0]):
                     continue
-            for y in range(image.shape[1]):
-                sy=int(y*sacle)
+            for y in range(resize_height):
+                sy=int(y/sacle)
                 if(sy >=image.shape[1] ):
                     continue
                 new_image[x,y]=image[sx,sy]
+
         return new_image.astype(np.uint8)
 
+    def bilinearInterpolation_transform(self,image,sacle):
+        resize_width=int(image.shape[0]*sacle)
+        resize_height=int(image.shape[1]*sacle)
+        new_image=np.zeros((resize_width,resize_height,3))
+        value=[0,0,0]
+        for i in range(resize_width):
+            for j in range(resize_height):
+                x = i/sacle
+                y = j/sacle
+                p=int((i+0.0)/sacle-x)
+                q=int((j+0.0)/sacle-y)
+                x=int(x)-1
+                y=int(y)-1
+                for k in range(3):
+                    if x+1<image.shape[0] and y+1<image.shape[1]:
+                        value[k]=int(image[x,y][k]*(1-p)*(1-q)+image[x,y+1][k]*q*(1-p)+image[x+1,y][k]*(1-q)*p+image[x+1,y+1][k]*p*q)
+                new_image[i, j] = (value[0], value[1], value[2])
+        return new_image.astype(np.uint8)
 
 if __name__ == '__main__':
-    max_count=5
+    max_count=7
     image = mpimg.imread('./image/lena.bmp')
     transformer=Transformation()
     
@@ -57,20 +79,25 @@ if __name__ == '__main__':
     temp.set_title('lena')
     plt.imshow(image)
 
-    # temp=fig.add_subplot(max_count,1,2)
-    # temp.set_title('offset')
-    # plt.imshow(transformer.offset_transform(image,-100,10))
+    temp=fig.add_subplot(max_count,1,2)
+    temp.set_title('offset')
+    plt.imshow(transformer.offset_transform(image,-100,10))
 
-    # temp=fig.add_subplot(max_count,1,3)
-    # temp.set_title('transport')
-    # plt.imshow(transformer.transport_transform(image))
+    temp=fig.add_subplot(max_count,1,3)
+    temp.set_title('transport')
+    plt.imshow(transformer.transport_transform(image))
 
-    # temp=fig.add_subplot(max_count,1,4)
-    # temp.set_title('mirror')
-    # plt.imshow(transformer.mirror_transform(image))
+    temp=fig.add_subplot(max_count,1,4)
+    temp.set_title('mirror')
+    plt.imshow(transformer.mirror_transform(image))
 
     temp=fig.add_subplot(max_count,1,5)
-    temp.set_title('mirror')
-    plt.imshow(transformer.scale_transform(image,2))
+    temp.set_title('nearestInterpolation_transform')
+    plt.imshow(transformer.nearestInterpolation_transform(image,2))
 
+    temp=fig.add_subplot(max_count,1,6)
+    temp.set_title('Bilinear_interpolation')
+    plt.imshow(transformer.bilinearInterpolation_transform(image,0.8))
+
+    
     plt.show()
